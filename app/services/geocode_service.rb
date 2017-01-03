daf2fd5010c5ab434188ed6159046766693628b7
Michael Scherer <michael.c.scherer@gmail.com>
2017-01-03 16:24:28 -0700

    Refactor geocode service

--- app/services/geocode_service.rb
+++ app/services/geocode_service.rb
@@ -1,15 +1,14 @@
 class GeocodeService
 
   def find_lat_long(address)
-    response = conn.get "maps/api/geocode/json?address=#{address}&key=#{ENV['GOOGLE_GEOCODE_KEY']}"
+    response = conn.get( '/maps/api/geocode/json', { address: address } )
     parsed = JSON.parse(response.body, symbolize_names: true)
   end
 
   def conn
     Faraday.new(:url => 'https://maps.googleapis.com') do |faraday|
-    faraday.request  :url_encoded
-    faraday.response :logger
-    faraday.adapter  Faraday.default_adapter
+      faraday.adapter  Faraday.default_adapter
+      faraday.params[:key] = ENV['GOOGLE_GEOCODE_KEY']
     end
   end
 
