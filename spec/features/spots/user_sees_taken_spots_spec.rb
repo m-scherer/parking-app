9ae7a15a230cfa7855e136ab1868a115cf794fc5
Michael Scherer <michael.c.scherer@gmail.com>
2017-01-05 09:35:32 -0700

    Label spots taken where taken? = true

--- spec/features/spots/user_sees_taken_spots_spec.rb
+++ spec/features/spots/user_sees_taken_spots_spec.rb
@@ -4,10 +4,12 @@ describe 'As a User' do
   let!(:spots) { create_list(:spot, 2) }
   context 'When a spot is taken' do
     it 'is labeled as taken' do
-      visit spots_path
-      spot = Spot.first.update(taken: true)
+      spot = Spot.first
+      spot.update(taken: true)
 
-      expect(page).to have_content(taken)
+      visit spots_path
+
+      expect(page).to have_content('Taken')
     end
   end
 end
