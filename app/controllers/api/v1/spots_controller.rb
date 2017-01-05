dfce21c3fcb872d9bca3cfd3c345e8dc730a1612
Michael Scherer <michael.c.scherer@gmail.com>
2017-01-05 15:16:52 -0700

    Add ability to change the taken flag based off of watson image recognition

--- app/controllers/api/v1/spots_controller.rb
+++ app/controllers/api/v1/spots_controller.rb
@@ -1,7 +1,24 @@
 class Api::V1::SpotsController < ApplicationController
 
   def update
+    classifiers = ImageClassifier.create_classifiers(get_image_url)
+    classifiers.each do |classifier|
+      if classifier.classification == 'vehicle' && classifier.score > 0.75
+        return find_spot.update!(taken: true)
+      else
+        find_spot.update(taken: false)
+      end
+    end
+  end
 
+  private
+
+  def get_image_url
+    params.permit(:image_url)[:image_url]
+  end
+
+  def find_spot
+    Spot.find(params.permit(:id)[:id])
   end
 
 end
--- spec/requests/api/v1/spot_update_taken_spec.rb
+++ spec/requests/api/v1/spot_update_taken_spec.rb
@@ -4,24 +4,29 @@ describe 'Spot API' do
   context 'PUT' do
     let!(:spot) { create(:spot) }
     it 'can update a spot as taken' do
-      params = { image_url: 'http://www.toyoland.com/photos/lexus/ls/LS-460.jpg' }
+      VCR.use_cassette('.take_spot') do
+        params = { image_url: 'http://www.toyoland.com/photos/lexus/ls/LS-460.jpg'}
 
-      expect(spot.taken).to eq(false)
+        expect(spot.taken).to eq(false)
 
-      put "/api/v1/spots/#{spot.id}", params: params
+        put "/api/v1/spots/#{spot.id}", params: params
 
-      expect(response).to be_success
-      expect(spot.taken).to eq(true)
+        expect(response).to be_success
+        expect(Spot.find(spot.id).taken).to eq(true)
+      end
     end
-    xit 'can update a spot as available' do
-      params = { image_url: 'http://jewishcurrents.org/old-site/wp-content/uploads/2016/07/parking-spot.jpg' }
+    it 'can update a spot as available' do
+      VCR.use_cassette('.spot_available') do
+        params = { image_url: 'http://jewishcurrents.org/old-site/wp-content/uploads/2016/07/parking-spot.jpg' }
 
-      expect(spot.taken).to eq(true)
+        spot.update(taken: true)
+        expect(spot.taken).to eq(true)
 
-      put "/api/v1/spots/#{spot.id}", params: params
+        put "/api/v1/spots/#{spot.id}", params: params
 
-      expect(response).to be_success
-      expect(spot.taken).to eq(false)
+        expect(response).to be_success
+        expect(Spot.find(spot.id).taken).to eq(false)
+      end
     end
   end
 end
