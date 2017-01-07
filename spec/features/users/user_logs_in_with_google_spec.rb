e5f1372e43ddbd2199b2c0db392d278fc6823aa2
Michael Scherer <michael.c.scherer@gmail.com>
2017-01-07 11:27:13 -0700

    Add login to navbar

--- spec/features/users/user_logs_in_with_google_spec.rb
+++ spec/features/users/user_logs_in_with_google_spec.rb
@@ -7,7 +7,7 @@ RSpec.feature "user logs in" do
     visit root_path
     click_link "Sign in with Google"
 
-    expect(page).to have_content('Michael Scherer')
+    expect(page).to have_content('scmi2nd@gmail.com')
     expect(page).to have_content('Logout')
   end
 end
